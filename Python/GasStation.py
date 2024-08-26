e9d169fec606fb8e5b72b50bbf950fa77b55f02c
Tuhinm2002 <75078694+Tuhinm2002@users.noreply.github.com>
2024-08-26 11:05:35 +0530

    Create GasStation.py

---
+++ Python/GasStation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        total = 0
+        res = 0
+        for i in range(len(gas)):
+            total += gas[i]-cost[i]
+            if total < 0:
+                res = i +1
+                total = 0
+        return res
